44def963b6bfd8efecc7e1d9188be88d609f1860
Denis Selimovic <denis.selimovic@klika.ba>
2021-10-04 23:04:38 +0200

    Basic structure for outputs

---
+++ link_gopher/output/base.py
@@ -0,0 +1,9 @@
+from abc import abstractmethod
+from typing import Generator, Protocol, Tuple
+
+
+class Output(Protocol):
+
+    @abstractmethod
+    def process(self, entires: Generator[Tuple[str, str], None, None]) -> None:
+        raise NotImplementedError()
