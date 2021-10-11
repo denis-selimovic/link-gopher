99f6c7ba2662b113f5c1aff9a13621fc0b42c670
Denis Selimovic <denis.selimovic@klika.ba>
2021-10-11 13:46:00 +0200

    Add validations for cliy

---
+++ link_gopher/cli/validations.py
@@ -0,0 +1,24 @@
+import click
+
+from link_gopher.output.factory import OutputFactory
+from link_gopher.source.factory import SourceFactory
+
+
+def validate_output(value, out):
+    output = OutputFactory.get(value)
+
+    if not output.is_valid(out):
+        raise click.BadParameter(
+            f'Unknown destination for output type {value}')
+
+    return value
+
+
+def validate_input(value, inpt):
+    input = SourceFactory.get(value)
+
+    if not input.is_valid(inpt):
+        raise click.BadParameter(
+            f'Unknown source for input type {value}')
+
+    return value
